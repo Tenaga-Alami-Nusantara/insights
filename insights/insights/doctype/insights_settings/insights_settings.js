c26bd6ca9e3d7469b47f525d79dbf190260772d3
Tri Putra Ardiyansah <trieputra@live.com>
2024-09-05 11:39:36 +0700

    feat(doctype): add new field

--- insights/insights/doctype/insights_settings/insights_settings.js
+++ insights/insights/doctype/insights_settings/insights_settings.js
@@ -1,7 +1,10 @@
 // Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Insights Settings', {
-	// refresh: function(frm) {
-	// }
-})
+frappe.ui.form.on("Insights Settings", {
+	refresh: function (frm) {
+		//Set app_icon_preview max width
+		$(frm.fields_dict.app_logo_preview.$img[0]).css("max-width", "10rem");
+		$(frm.fields_dict.favicon_preview.$img[0]).css("max-width", "10rem");
+	},
+});
